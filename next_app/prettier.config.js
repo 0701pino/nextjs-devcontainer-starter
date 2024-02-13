cc6311c96a9aed1bde409ec71c9fe335d1da77d6
pino <0701pino@kumonekodo.com>
2024-02-13 14:42:33 +0000

    importを自動でソートされる設定を追加

--- next_app/prettier.config.js
+++ next_app/prettier.config.js
@@ -1,3 +1,33 @@
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+/** @typedef {import("prettier").Config} PrettierConfig */
+
+/**
+ * Remember to restart VSCode after making
+ * any changes here and saving this file.
+ */
+/** @type {PrettierConfig | SortImportsConfig} */
 module.exports = {
   printWidth: 100,
+  plugins: ["@ianvs/prettier-plugin-sort-imports"],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "",
+    "^(^react$|@react|react)",
+    "^(next/(.*)$)|^(next$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "^@[^/](.*)$",
+    "",
+    "^@/(.*)$",
+    "^[../]",
+    "^[./]",
+    "",
+    "<TYPES>",
+    "<TYPES>^[.]",
+    "",
+    "^(?!.*[.]css$)[./].*$",
+    ".css$",
+  ],
+  importOrderParserPlugins: ["typescript", "jsx"],
+  importOrderTypeScriptVersion: "5.3.3",
 };
