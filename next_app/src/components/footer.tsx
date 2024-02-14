2d765abaedfd007dec7b08257cd2e605cb25b31a
pino <0701pino@kumonekodo.com>
2024-02-14 14:03:46 +0000

    画面構成をheader/main/footerに変更して、ソースの整理を行った

---
+++ next_app/src/components/footer.tsx
@@ -0,0 +1,15 @@
+// components/Footer.tsx
+
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-200 p-10 ">
+      <div className="container mx-auto text-center">
+        <p>Footer</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ next_app/src/components/header.tsx
@@ -0,0 +1,15 @@
+// components/Header.tsx
+
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-blue-100  p-5">
+      <div className="mx-auto text-center">
+        <h1 className="text-xl">Header</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
